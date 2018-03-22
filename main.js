db6037bc553f3bf9ad58ec6b96f3ec5fe2e659c6
Renzzauw <renzo@schindeler.nl>
2018-03-22 16:22:05 +0100

    form dingen

--- main.js
+++ main.js
@@ -3,24 +3,6 @@
 var http = require("http");
 var url = require("url");
 
-/*
-http.createServer(function (request, response) {
-    
-    var parsedUrl = url.parse(request.url, true); // true - query string as object
-    var queryAsObject = parsedUrl.query; // to get the query string
-    var name = queryAsObject.name; // to get the target value
-    
-    response.writeHead(200, {'Content-Type': "text/html"});
-    
-    response.write('<html><body>');
-    response.write('<h1>Welcome!</h1>');
-    response.write('<p>Welcome to our site that is utter crap at the moment.</p>');
-    response.end('</html></body>');
-    
-    //response.end('Hello, ' + name + '\n'); // personalized hello
-}).listen(8010);
-*/
-
 http.createServer(function(request, response) {
     request.on('error', function(err) {
         console.error(err);
